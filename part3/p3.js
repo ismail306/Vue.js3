daf392a05c0d22299ceb8882bf58791bc8f2fe7c
ismail306 <ismail.diuswe@gmail.com>
2023-08-03 15:23:52 +0600

    class 1,2,3 done

---
+++ part3/p3.js
@@ -0,0 +1,13 @@
+var app = Vue.createApp({
+  data() {
+    return {
+      name: "Ismail Khan Emon",
+      obj1: {
+        name: "Ismail Khan Emon",
+        age: 25,
+        github: "//github.com/ismail306",
+      },
+    };
+  },
+});
+app.mount("#app");
