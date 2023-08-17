1bfde70cba4d20c0477327ebb03d681024799145
ismail306 <ismail.diuswe@gmail.com>
2023-08-17 17:56:58 +0600

    Food Item Done

---
+++ Scaling/firstsfc/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Food</h1>
+  <food-item 
+  v-for="x in foods"
+:food-name="x"
+  />
+
+ 
+</template>
+
+<script>
+
+export default{
+  data(){
+    return {
+      foods :["Apple","Mango","Banana",]
+    };
+
+  },
+
+}
+
+</script>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+    cursor: pointer;
+  }
+</style>
---
+++ Scaling/firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h2>{{ foodName }}</h2>
+      <h1>{{ foodDescription }}</h1>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      props: [
+        'foodName'
+      ]
+    }
+  </script>
+  
+  <style></style>
